c48cf034e978085329764aa64f9e0a2e43ae7f4f
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-10 23:36:33 +0300

    Update Registreeriminejaautoriseerimine.py

--- Registreeriminejaautoriseerimine.py
+++ Registreeriminejaautoriseerimine.py
@@ -1,26 +1,19 @@
 from MyModule import* 
 import random
-user=["libol11","kolfi5471","gbjfh4"]
-pasword=["GGFhjKI11","FFbhgr45","geYU47yh"]
+login=["Kolomoiets"]
+pasword=["tijoy"]
 print("****Registreerimine või autoriseerimine***")
 while True:
-	print("1-Registreerimine;2-Autoriseerimine;3-Logi välja")
+	print("1-Registreerimine,2-autoriseerimine,3-Logi välja")
 	n=int(input())
 	if n==1:
 		print("Registreerimine")
-		if n==1:
-			login=input("Nimi:")
-			if login not in user:
-				print("Nimi sobib!")
-			break
-		else:
-			print("Kasutajanimi on juba võetud!!!!")
-		m=input("Automaatne parooli loomine(N);Sõltumatu(K)")
-		if m.upper()=="N":
-			pas=apass()
-		if m.upper()=="K":
-			pas=input("Sisestage oma parool -> ")
-			kont=passkontroll(password)
-
+		while True:
+			print("4-autoreg","5-isereg")
+			a=int(input())
+			if a==4:
+				print(pas)
+			if a==5:
+				print(pass1)
 			
 	
