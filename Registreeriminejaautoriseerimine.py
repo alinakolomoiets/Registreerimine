7479112a7a61dbfbc673b67bc932e522368a2077
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-12 10:45:12 +0200

    Update Registreeriminejaautoriseerimine.py

--- Registreeriminejaautoriseerimine.py
+++ Registreeriminejaautoriseerimine.py
@@ -3,17 +3,7 @@ import random
 login=["Kolomoiets"]
 pasword=["tijoy"]
 print("****Registreerimine või autoriseerimine***")
-while True:
-	print("1-Registreerimine,2-autoriseerimine,3-Logi välja")
-	n=int(input())
-	if n==1:
-		print("Registreerimine")
-		while True:
-			print("4-autoreg","5-isereg")
-			a=int(input())
-			if a==4:
-				print(pas)
-			if a==5:
-				print(pass1)
+
+men()
 			
 	
