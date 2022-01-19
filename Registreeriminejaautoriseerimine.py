a9a7e145880e9d897495230b7b0d77ece0fb2a5b
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-19 09:40:19 +0200

    Update Registreeriminejaautoriseerimine.py

--- Registreeriminejaautoriseerimine.py
+++ Registreeriminejaautoriseerimine.py
@@ -1,9 +1,24 @@
 from MyModule import* 
 import random
-login=["Kolomoiets"]
-pasword=["tijoy"]
+user=[]
+user=failist_lugemine('TextFile1.txt',user)
+print(user)
+
+pasword=[]
+pasword=failist_lugemine('Pasword.txt',pasword)
+print(pasword)
+
 print("****Registreerimine või autoriseerimine***")
 
 men()
+			
+
+user=rida_salvestamine("TextFile1.txt",user)
+print(user)
+
+pasword=rida_salvestamine("Pasword.txt",pasword)
+print(pasword)
+			
+
 			
 	
