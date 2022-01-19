5458f4d26dd4c77c80fd7e9f615b19b4b8902aa1
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-01-19 09:39:51 +0200

    Update MyModule.py

--- MyModule.py
+++ MyModule.py
@@ -1,6 +1,40 @@
 import random
 login=[]
 password=[]
+def failist_lugemine(f:str,l:list):
+	"""Info failist f listisse l
+	"""
+	fail=open(f,'r')#,encoding="utf-8-sig"
+	for rida in fail:
+		l.append(rida.strip())#'\n'
+	fail.close()
+	return l
+
+def failist_lugemine(f:str,l:list):
+	"""Info pasword
+	"""	
+	fail=open(f,'r')
+	for rid in fail:
+		l.append(rid.strip())
+	fail.close()
+	return l
+
+def failisse_salvestamine(f:str,l:list):
+	"""Loetelu salvaestame failisse
+	"""	
+	fail=open(f,'w')#перезапись
+	for el in l:
+		fail.write(el+'\n')
+	fail.close()
+
+def rida_salvestamine(f:str,rida:str):
+	"""Üks sõna või lause (rida) salvestame failisse
+    :param str f:fail kuku salvastame
+    :param str rida :sõna , mis vaja salvestada failisse
+    """
+	fail=open(f,'a')#до записываем остается то что было и добавляется новое 
+	fail.write(rida+'\n')
+	fail.close()
 def men():
     while True:
         print("1-Registreerimine")
